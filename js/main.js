1664b5a6e1accfc3996cc55749a43e6054f9bc56
Rettus code <michael.rettusii@gmail.com>
2020-11-11 06:28:34 -0800

    ts5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
-const form1 = document.querySelector('.feedback')
+// const form1 = document.querySelector('.feedback')
 
-form1.addEventListener('submit', e => {
-    e.preventDefault();
+// form1.addEventListener('submit', e => {
+//     e.preventDefault();
 
-    const sub = form1.subscribe.value;
-    if (sub == '') {
-        alert("Enter a Valid Name!");
-        form1.subscribe.style.border = "1px solid crimson";
-        return false;
-    }
-    form1.submit();
-})
+//     const sub = form1.subscribe.value;
+//     if (sub == '') {
+//         alert("Enter a Valid Name!");
+//         form1.subscribe.style.border = "1px solid crimson";
+//         return false;
+//     }
+//     form1.submit();
+// })
 
 
