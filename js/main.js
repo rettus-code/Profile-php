b165be9a9bb1d86943f42453f9cbf6a71613f24f
Rettus code <michael.rettusii@gmail.com>
2020-11-15 09:24:22 -0800

    add testimonials

--- js/main.js
+++ js/main.js
@@ -18,17 +18,16 @@ $(function(){
         loop:true,
         margin:20,
         nav:true,
+        dotsEach: true,
         responsive:{
             0:{
                 items:1,
             },
             600:{
                 items:2,
-            },
-            1000:{
-                items:3,
             }
         },
     });
 });
+
 
